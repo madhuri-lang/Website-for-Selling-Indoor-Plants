f99612de93929d54581aad8b34e00928441251fa
Vakkala Madhuri <66001343+madhuri-lang@users.noreply.github.com>
2021-12-20 11:58:05 +0530

    Create app.component.ts

---
+++ Frontend/app-component/app.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { AuthGuardService } from './auth-guard.service';
+import { AuthenticationService } from './authentication.service';
+import { CartService } from './cart.service';
+import { Cart2Service } from './cart2.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'routing-app';
+
+  public totalItem : number=0;
+  cartProducts:any=[];
+  cartLength=0;
+  constructor(private authenticationService : AuthenticationService , private authguard:AuthGuardService, private cartService:Cart2Service){}
+  valLogin= false;
+  ngDoCheck()
+  {
+    if(localStorage.getItem('isLoggedIn')=='true')
+    {
+      this.valLogin=true;
+    }
+  }
+
+  
+  logInForAuthGuard()
+  {
+    this.authenticationService.login();
+     if(this.authenticationService.logVal==true)
+
+     this.valLogin= true;
+    
+  }
+  logOutForAuthGuard()
+  {
+    this.authenticationService.logout();
+     if(this.authenticationService.logVal==false)
+     this.valLogin= false;
+  }
+
+  readCart()
+    {
+      this.cartService.getProducts().subscribe(
+        (data)=>{
+          this.cartProducts= data;
+          console.log("cart items length:"+this.cartProducts.length);
+          this.cartLength=this.cartProducts.length;
+        },
+        (err)=>{
+          console.log(err);
+        }
+      )
+    }
+
+    // cartLengthFunc():number
+    // {
+    //   this.readCart();
+    //   return this.cartLength;
+    // }
+
+
+ngOnInit():void{
+  
+}
+}
